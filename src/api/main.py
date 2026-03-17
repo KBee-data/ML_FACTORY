c637b0af2f1ae88d936bc347f7b0bc39cfa86b40
KBee-Data <kimberly.n.black@gmail.com>
2026-03-17 12:01:33 +0100

    - added Streamlit frontend - check of API running locally = OK - added api and streamlit services to docker-compose - added/updated dockerfiles

---
+++ src/api/main.py
@@ -0,0 +1,82 @@
+# src/api/main.py
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import os
+from dotenv import load_dotenv
+import mlflow
+from mlflow import MlflowClient
+
+# Load environment variables
+
+load_dotenv() # reads .enc in project root
+
+MLFLOW_URI = os.getenv("MLFLOW_TRACKING_URI")
+MODEL_NAME = "iris_model"
+MODEL_ALIAS = "Production"
+
+app = FastAPI(title="ML Factory API")
+
+# MLflow client
+client = MlflowClient(tracking_uri=MLFLOW_URI)
+
+# cache for hot-reloading
+state = {
+    "model": None,
+    "version": None
+}
+
+# Input SCHEMAS for the API
+class IrisInput(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+# utilities
+def load_production_model():
+    """Load current Production model from MLflow"""
+
+    try:
+        alias_info = client.get_model_version_by_alias(MODEL_NAME, MODEL_ALIAS)
+        prod_version = alias_info.version
+
+        if state["model"] is None or state["version"] != prod_version:
+            print(f"Loading model version {prod_version} from MLflow...")
+            model_uri =f"models:/{MODEL_NAME}@{MODEL_ALIAS}"
+            state["model"] = mlflow.pyfunc.load_model(model_uri)
+            state["version"] = prod_version
+        
+        return state["model"], state["version"]
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=f"MLflow error:{str(e)}")
+    
+# routes
+@app.get("/")
+def api_landing():
+    return {"ML Factory API is up and running"}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+@app.post("/predict")
+def predict(input_data: IrisInput):
+    model, version = load_production_model()
+    features = [[
+        input_data.sepal_length,
+        input_data.sepal_width,
+        input_data.petal_length,
+        input_data.petal_width
+    ]]
+    prediction = model.predict(features)
+    #probabilities = model.predict_proba(features)
+
+    IRIS_CLASSES = ["setosa", "versicolor", "virginica"]
+
+    return {
+        "prediction": int(prediction[0]),
+        #"probabilities": probabilities[0],
+        "species": IRIS_CLASSES[int(prediction[0])],
+        "model_version": version
+    }
---
+++ src/front/streamlit_app.py
@@ -0,0 +1,42 @@
+# src/front/streamlit_app.py
+
+import streamlit as st
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_URL = os.getenv("API_URL_PREDICT")
+
+st.title("Iris Prediction App")
+
+st.write("Enter flower measurements")
+
+sepal_length = st.number_input("Sepal length")
+sepal_width = st.number_input("Sepal width")
+petal_length = st.number_input("Petal length")
+petal_width = st.number_input("Petal width")
+
+if st.button("Predict"):
+    payload = {
+        "sepal_length": sepal_length,
+        "sepal_width": sepal_width,
+        "petal_length": petal_length,
+        "petal_width": petal_width
+    }
+
+    response = requests.post(API_URL, json=payload)
+
+    if response.status_code == 200:
+        result = response.json()
+
+        st.success(f"Species: {result['species']}")
+        st.write(f"Predicted class: {result['prediction']}")
+        #st.write(f"Prediction probability: {result['probabilities']}")
+        st.write("Model version:", result["model_version"])
+
+    else:
+        st.error(f"API error, API response = {response.status_code}")
+
+        
